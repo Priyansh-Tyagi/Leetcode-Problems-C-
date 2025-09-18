7384f2d690d1133df61e128993f733e260c7c4fd
Priyansh <priyanshdev09@gmail.com>
2025-09-18 19:32:42 +0530

    18/09/2005

---
+++ Tree_binary_traversal.cpp
@@ -0,0 +1,72 @@
+
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+
+        int data;
+        Node* left;
+        Node* right;
+    Node(int value){
+      data = value;
+      left=right=nullptr;
+    }
+};
+
+    void preorder(Node* root){
+        if(root == NULL ){
+            return;
+        }
+        cout<<root->data<<" ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+void inorder(Node* root){
+    if(root == NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+void postorder(Node* root){
+    if(root == NULL){
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+    
+}
+
+
+
+int main() {
+    
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    
+    root->left->left= new Node(4);
+    root->left->left->left= new Node(6);
+    root->right->right= new Node(5);
+    root->right->right->left= new Node(7);
+    root->right->right->right= new Node(8);
+    root->right->right->right->left= new Node(9);
+    
+    cout<<"preorder ";
+    preorder(root);
+    cout<<endl;
+    
+    cout<<"inorder ";
+    inorder(root);
+    cout<<endl;
+    
+    cout<<"postorder ";
+    postorder(root);
+    cout<<endl;
+    
+    
+    
+    return 0;
+}
